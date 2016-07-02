3a9b0857db82abdf6dec96b16b02c39573a77943
manager <123456@126.com>
2016-07-03 00:36:37 +0800

    项目的初始化处理

---
+++ CCDemoCode/CCDemoCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CCDemoCode
+//
+//  Created by 陈驰 on 16/7/3.
+//  Copyright © 2016年 陈驰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CCDemoCode/CCDemoCode/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  CCDemoCode
+//
+//  Created by 陈驰 on 16/7/3.
+//  Copyright © 2016年 陈驰. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
